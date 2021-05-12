cfe9e0cf35bdff38191fc2ffe49d1e72e06c0350
itpark <itpark.team@gmail.com>
2021-05-12 21:36:48 +0300

    almost task voroni

--- src/com/company/Main4.java
+++ src/com/company/Main4.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main4 {
     public static void main(String[] args) {
         //на вход вводиться кол-во ворон (целое)
@@ -7,5 +9,31 @@ public class Main4 {
         //1-ворона, 10000001 - ворона
         //4-вороны, 584 - вороны
         //switch
+        //1 - ворона
+        //2,3,4 - вороны
+        //0,5,6,7,8,9 - ворон
+
+        Scanner input = new Scanner(System.in);
+        int countOfRaven;
+
+        System.out.print("сколько на поле прилетело ворон?: ");
+        countOfRaven = input.nextInt();
+
+        int lastDigit = countOfRaven % 10;
+
+        switch (lastDigit){
+            case 1:
+                System.out.print("ворона");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.print("вороны");
+                break;
+            default:
+                System.out.print("ворон");
+                break;
+        }
+
     }
 }
