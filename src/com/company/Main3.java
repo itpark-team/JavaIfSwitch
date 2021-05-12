3609883deba48242004abd9a624a094b26731dfc
itpark <itpark.team@gmail.com>
2021-05-12 21:09:34 +0300

    done switch

---
+++ src/com/company/Main3.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int dayOfWeek;
+
+        System.out.print("input dayOfWeek: ");
+        dayOfWeek = input.nextInt();
+
+        switch (dayOfWeek)
+        {
+            case 1:
+                System.out.println("pn");
+                break;
+            case 2:
+                System.out.println("vt");
+                break;
+            case 3:
+                System.out.println("sr");
+                break;
+            case 4:
+                System.out.println("cht");
+                break;
+            case 5:
+                System.out.println("pt");
+                break;
+            case 6:
+                System.out.println("sb");
+                break;
+            case 7:
+                System.out.println("vsk");
+                break;
+            default:
+                System.out.println("takogo dnya net");
+                break;
+        }
+    }
+}
